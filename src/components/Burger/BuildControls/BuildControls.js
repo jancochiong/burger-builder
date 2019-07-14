f5428903b373d5b3bb278fc37e5fa4ff6b8d5e73
Jan Co Chiong <jan.cochiong@gmail.com>
2019-07-14 13:54:04 -0400

    add buy now button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -23,6 +23,11 @@ const buildControls = (props) => (
                     />
                 ))
             }
+            <button 
+                className={classes.OrderButton}
+                disabled={!props.purchasable}>
+                ORDER NOW
+            </button>
         </div>
 );
 
